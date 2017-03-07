415b0eaf9baedf3a74175a5135cca4c8bd553e8d
Hauke Stieler <mail@hauke-stieler.de>
2017-03-07 23:14:44 +0100

    Added java files

---
+++ src/juard/PreconditionNotNullFailed.java
@@ -0,0 +1,15 @@
+package juard;
+
+/**
+ * @author hauke
+ */
+public class PreconditionNotNullFailed extends RuntimeException
+{
+	private static final long serialVersionUID = 1L;
+
+	@SuppressWarnings("javadoc")
+	public PreconditionNotNullFailed()
+	{
+		super("The given argument must not be null but is was.");
+	}
+}
