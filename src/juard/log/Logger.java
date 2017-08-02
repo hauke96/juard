306b846757cb4b6775e650999b8a7146fdb3244a
Hauke Stieler <mail@hauke-stieler.de>
2017-08-02 23:17:06 +0200

    Rename methods and fields and add warning-method

--- src/juard/log/Logger.java
+++ src/juard/log/Logger.java
@@ -14,92 +14,112 @@ import java.util.Date;
  */
 public class Logger
 {
-	private static final String	DEBUG	= "[debug]";
-	private static final String	ERROR	= "[error]";
-	private static final String	INFO	= "[info] ";
-	private static final String	FATAL	= "[fatal]";
+	private static final String	DEBUG			= "[debug]";
+	private static final String	INFO			= "[info]  ";
+	private static final String	WARN			= "[warn]  ";
+	private static final String	ERROR			= "[error]";
+	private static final String	FATAL			= "[fatal]";
+	private static final String	newLineSpaces	= "                 ";
 	
 	/**
 	 * True to create an empty line between each output.
 	 */
-	public static boolean	__separatingEmptyLine	= false;
+	public static boolean	separatingEmptyLine	= false;
 	/**
 	 * True to also show debug output.
 	 */
-	public static boolean	__showDebug				= true;
+	public static boolean	showDebug			= true;
 	
 	/**
-	 * Prints the given information including
+	 * Prints the given information onto the console with the date and the [info]-tag.
 	 * 
 	 * @param infoMessage
 	 *            The message to print as normal information.
 	 */
-	public static void __info(String infoMessage)
+	public static void info(String infoMessage)
 	{
-		__printOutput(infoMessage, INFO, System.out);
+		printOutput(infoMessage, INFO, System.out);
+	}
+	
+	/**
+	 * Prints the given information onto the console with the date and the [warn]-tag.
+	 * 
+	 * @param infoMessage
+	 *            The message to print as normal information.
+	 */
+	public static void warn(String infoMessage)
+	{
+		printOutput(infoMessage, WARN, System.out);
 	}
 	
 	/**
 	 * Prints a debug message into the console but only if the debug mode is on.
+	 * This includes the date, [debug]-tag and the class:line information where the method was called from.
 	 * 
 	 * @param debugMessage
 	 *            The message to print in debug mode.
 	 */
-	public static void __debug(String debugMessage)
+	public static void debug(String debugMessage)
 	{
-		if (!__showDebug) return;
-		// The 3 will print the path to the line of code that calls __debug(String)
-		__printOutput(debugMessage, DEBUG, System.out, 3);
+		if (!showDebug) return;
+		// The 3 will print the path to the line of code that calls debug(String)
+		printOutput(debugMessage, DEBUG, System.out, 3);
 	}
 	
 	/**
 	 * Prints the error message to STD_ERR.
+	 * This includes the date, [error]-tag and the class:line information where the method was called from.
 	 * 
 	 * @param errorMessage
 	 *            The message to show as error.
 	 */
-	public static void __error(String errorMessage)
+	public static void error(String errorMessage)
 	{
-		__printOutput(errorMessage, ERROR, System.err, 3);
+		printOutput(errorMessage, ERROR, System.err, 3);
 	}
 	
 	/**
 	 * Prints the error message to STD_ERR.
+	 * This includes the date, [error]-tag and the class:line information where the method was called from.
+	 * After that comes the stack trace of the throwable passed.
 	 * 
 	 * @param errorMessage
 	 *            The message to show as error.
 	 * @param throwable
 	 *            An throwable object to show even more information.
 	 */
-	public static void __error(String errorMessage, Throwable throwable)
+	public static void error(String errorMessage, Throwable throwable)
 	{
-		__printOutput(errorMessage, ERROR, System.err, 3);
+		printOutput(errorMessage, ERROR, System.err, 3);
 		throwable.printStackTrace();
 	}
 	
 	/**
 	 * Shows the error message and exits the application with exit-code 1.
+	 * The printed text includes the date, [fatal]-tag and the class:line information where the method was called from.
 	 * 
 	 * @param errorMessage
 	 *            The error message to show as fatal error.
 	 */
-	public static void __fatal(String errorMessage)
+	public static void fatal(String errorMessage)
 	{
-		__printOutput(errorMessage, FATAL, System.err, 3);
+		printOutput(errorMessage, FATAL, System.err, 3);
 		System.exit(1);
 	}
 	
 	/**
 	 * Shows the error message and exits the application with exit-code 1.
+	 * The printed text includes the date, [fatal]-tag and the class:line information where the method was called from.
+	 * After that comes the stack trace of the throwable passed.
 	 * 
 	 * @param errorMessage
 	 *            The error message to show.
 	 * @param throwable
 	 *            An throwable object to show even more information.
 	 */
-	public static void __fatal(String errorMessage, Throwable throwable)
+	public static void fatal(String errorMessage, Throwable throwable)
 	{
-		__printOutput(errorMessage, FATAL, System.err, 3);
+		printOutput(errorMessage, FATAL, System.err, 3);
 		throwable.printStackTrace();
 		System.exit(1);
 	}
@@ -114,9 +134,9 @@ public class Logger
 	 * @param stream
 	 *            The output stream (STD_ERR or STD_OUT)
 	 */
-	private static void __printOutput(String message, String tag, PrintStream stream)
+	private static void printOutput(String message, String tag, PrintStream stream)
 	{
-		__printOutput(message, tag, stream, -1);
+		printOutput(message, tag, stream, -1);
 	}
 	
 	/**
@@ -131,7 +151,7 @@ public class Logger
 	 * @param stackCounter
 	 *            The stack counter. Set this to -1 to not display the method that called uses the Logger.
 	 */
-	private static void __printOutput(String message, String tag, PrintStream stream, int stackCounter)
+	private static void printOutput(String message, String tag, PrintStream stream, int stackCounter)
 	{
 		if (message.length() > 0)
 		{
@@ -148,7 +168,7 @@ public class Logger
 			}
 			else
 			{
-				formattedOutput = MessageFormat.format("{0} {1} {2}\n                 ", dateFormat.format(date), tag, Thread.currentThread().getStackTrace()[stackCounter]);
+				formattedOutput = MessageFormat.format("{0} {1} {2}\n{3}", dateFormat.format(date), tag, Thread.currentThread().getStackTrace()[stackCounter], newLineSpaces);
 			}
 			stream.print(formattedOutput);
 			
@@ -162,12 +182,12 @@ public class Logger
 				for (int i = 1; i < splittedMessages.length; i++)
 				{
 					line = splittedMessages[i];
-					line = "                 " + line;
+					line = newLineSpaces + line;
 					stream.println(line);
 				}
 			}
 			
-			if (__separatingEmptyLine)
+			if (separatingEmptyLine)
 			{
 				System.out.println();
 			}
